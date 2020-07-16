1d6b7424b1b32d99adc20cca26de0fd21669ab9d
liziczh <liziczh@foxmail.com>
2020-07-16 23:29:43 +0800

    TUser

---
+++ app-sso-api/src/main/java/com/liziczh/app/sso/api/condition/UserCondition.java
@@ -0,0 +1,23 @@
+package com.liziczh.app.sso.api.condition;
+
+import java.util.Date;
+
+import com.liziczh.base.common.condition.BaseCondition;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class UserCondition extends BaseCondition {
+	private static final long serialVersionUID = -7473402179827602416L;
+	private Integer id;
+	private String name;
+	private String email;
+	private String mobile;
+	private String idCard;
+	private String address;
+	private Date startTime;
+	private Date endTime;
+	private String valid;
+}
